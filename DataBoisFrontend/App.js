ec054a3dfa5f91d04bd79f3941f0a4f7cc150c7b
unknown <kyleallan07@gmail.com>
2022-04-27 01:37:50 +0800

    Update App.js and Pages
    Cart and Checkout Pages

--- DataBoisFrontend/App.js
+++ DataBoisFrontend/App.js
@@ -8,7 +8,10 @@ const Stack = createStackNavigator();
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Home from "./Pages/Home";
-
+import Cart from "./Pages/Cart";
+import CheckOut from "./Pages/CheckOut";
+import Headphone1 from "./Pages/ItemsPage/Headphone1"
+import Headphone2 from "./Pages/ItemsPage/Headphone2"
 
 function App() {
   return (
@@ -29,7 +32,27 @@ function App() {
           name="Home"
           component={Home}
         />
-        
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Cart"
+          component={Cart}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="CheckOut"
+          component={CheckOut}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Headphone1"
+          component={Headphone1}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Headphone2"
+          component={Headphone2}
+        />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
